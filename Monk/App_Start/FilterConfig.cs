2baae67fff2962c37db76ac56f848723be2beff4
Alex Wainshtein <w.alex2k@gmail.com>
2018-01-19 19:01:20 +0200

    Add project files.

---
+++ Monk/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Monk
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
