25000c0f5fe5811d94e7565f7bbeeb05e1ed97c6
Alex Wainshtein <w.alex2k@gmail.com>
2018-01-19 23:16:20 +0200

    New Monk project and finall name :D

---
+++ Monk/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Monk.Models;
+namespace Monk.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get: "/Account/AddWorker?FirstName="+firstName+"&SirName="+sirName+"&IdCard="+idCard+"&Phone="+phone+"&email="+email
+        public bool AddWorker(string firstName, string sirName, string idCard, string phone, string email)
+        {
+            bool added = false;
+            bool exsits = false;
+            using (var db = new WorkerContext())
+            {
+                foreach (var item in db.Workers)
+                {
+                    if(item.IdCard == idCard)
+                    {
+                        exsits = true;
+                    }
+                }
+                if (!exsits)
+                {
+                    db.Workers.Add(new Worker { FirstName = firstName, SirName = sirName, IdCard = idCard, Phone = phone, Email = email });
+                    db.SaveChanges();
+                    added = true;
+                }
+            }
+            return added;
+
+        }
+        public ActionResult WorkerAdd()
+        {
+            return View();
+        }
+        public ActionResult WorkerTable()
+        {
+            var db = new WorkerContext();
+            return View(db.Workers);
+            
+        }
+
+
+        public void DeleteWorker()
+        {
+            using (var db = new WorkerContext())
+            {
+                db.Database.Delete();
+            }
+        }
+    }
+}
---
+++ Monk/Models/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Monk.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SirName { get; set; }
+        public string IdCard { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+
+    }
+     
+    class WorkerContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+    }
+}
